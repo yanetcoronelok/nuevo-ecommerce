d441bff4cd0a7723af6a3eed5f9cba515b5efc75
yanetcoronelok <ya.coronel@gmail.com>
2024-10-19 14:54:34 -0300

    Finalizacion de los productos

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const PRODUCTOS = [
         titulo: "Foto 01",
         imagen: "../resources/photographies/01.jpg",
         categoria: {
-            nombre: "cities",
+            nombre: "Cities",
             id: "cities"
         },
         precio: 10
@@ -14,7 +14,7 @@ const PRODUCTOS = [
         titulo: "Foto 02",
         imagen: "../resources/photographies/02.jpg",
         categoria: {
-            nombre: "cities",
+            nombre: "Cities",
             id: "cities"
         },
         precio: 10
@@ -24,7 +24,7 @@ const PRODUCTOS = [
         titulo: "Foto 03",
         imagen: "../resources/photographies/03.jpg",
         categoria: {
-            nombre: "portraits",
+            nombre: "Portraits",
             id: "portraits"
         },
         precio: 10
@@ -34,7 +34,7 @@ const PRODUCTOS = [
         titulo: "Foto 04",
         imagen: "../resources/photographies/04.jpg",
         categoria: {
-            nombre: "events",
+            nombre: "Events",
             id: "events"
         },
         precio: 10
@@ -44,7 +44,7 @@ const PRODUCTOS = [
         titulo: "Foto 05",
         imagen: "../resources/photographies/04.jpg",
         categoria: {
-            nombre: "events",
+            nombre: "Events",
             id: "events"
         },
         precio: 10
@@ -54,18 +54,22 @@ const PRODUCTOS = [
         titulo: "Foto 06",
         imagen: "../resources/photographies/05.jpg",
         categoria: {
-            nombre: "portraits",
+            nombre: "Portraits",
             id: "portraits"
         },
         precio: 10
     }
 ];
 
-const contenedorProductos = document.querySelector("#contenedor-productos");
+const CONTENEDOR_PRODUCTOS = document.querySelector("#contenedor-productos");
+const BOTONES_CATEGORIAS = document.querySelectorAll(".boton-categoria");
+const TITULO_PRINCIPAL = document.querySelector("#titulo-principal");
 
-function cargarProductos(){
+function cargarProductos(productosElegidos){
 
-    PRODUCTOS.forEach(producto => {
+    CONTENEDOR_PRODUCTOS.innerHTML = "";
+
+    productosElegidos.forEach(producto => {
 
         const div = document.createElement("div");
         div.classList.add("producto");
@@ -78,9 +82,30 @@ function cargarProductos(){
                     </div>
                  `;
 
-        contenedorProductos.append(div);
+                 CONTENEDOR_PRODUCTOS.append(div);
     })
 
 }
 
-cargarProductos();
+cargarProductos(PRODUCTOS);
+
+BOTONES_CATEGORIAS.forEach(boton => {
+    boton.addEventListener("click", (e) =>{
+        debugger
+        BOTONES_CATEGORIAS.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+
+        if (e.currentTarget.id != "todos"){
+            const PRODUCTO_CATEGORIA = PRODUCTOS.find(producto => producto.categoria.id === e.currentTarget.id);
+            
+            TITULO_PRINCIPAL.innerText = PRODUCTO_CATEGORIA.categoria.nombre;
+
+            const PRODUCTOS_BOTON = PRODUCTOS.filter(producto => producto.categoria.id ===e.currentTarget.id);
+            cargarProductos(PRODUCTOS_BOTON);
+    } else {
+        TITULO_PRINCIPAL.innerText = "All Products";
+        cargarProductos(PRODUCTOS);
+    }
+    })
+})
+
